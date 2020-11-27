c175856d283c6087d889b5a6e1e94c2363354fbb
RollsRoycewk <50765560@qq.com>
2020-11-27 21:07:59 +0800

    feat:项目初始化

---
+++ src/views/Home/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    劳斯宾利法拉利
+    <p>北京京北会 傲途汇 京车汇</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+};
+</script>
+
+<style lang="less" scoped>
+.home {
+  background-color: red;
+  p {
+    font-size: 50px;
+  }
+}
+</style>
+
