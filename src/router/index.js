a2a7789431c5e893cee0400b05a628fabf9d755c
RollsRoycewk <50765560@qq.com>
2020-11-29 18:51:33 +0800

    feat:修改push和replace

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,19 @@ import Login from "../views/Login";
 import Register from "../views/Register";
 import Search from "../views/Search";
 
+const push = VueRouter.prototype.push;
+const replace = VueRouter.prototype.replace;
+
+VueRouter.prototype.push = function(location, onComplete, onAbort = () => {}) {
+  return push.call(this, location, onComplete, onAbort);
+};
+VueRouter.prototype.replace = function(
+  location,
+  onComplete,
+  onAbort = () => {}
+) {
+  return replace.call(this, location, onComplete, onAbort);
+};
 Vue.use(VueRouter);
 
 export default new VueRouter({
