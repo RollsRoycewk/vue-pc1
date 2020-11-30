478773a8a86053aff89f7df1a41f0aeef152d657
RollsRoycewk <50765560@qq.com>
2020-11-30 23:54:26 +0800

    feat:显示隐藏

--- src/components/TypeNav/index.vue
+++ src/components/TypeNav/index.vue
@@ -1,9 +1,17 @@
 <template>
   <!-- 商品分类导航 -->
   <div class="type-nav">
-    <div class="container">
+    <div
+      class="container"
+      @mouseenter="isLoginShow = true"
+      @mouseleave="isLoginShow = false"
+    >
       <h2 class="all">全部商品分类</h2>
-      <nav class="nav">
+      <nav
+        class="nav"
+        @mouseenter="isLoginShow = true"
+        @mouseleave="isLoginShow = false"
+      >
         <a href="###">服装城</a>
         <a href="###">美妆馆</a>
         <a href="###">尚品汇超市</a>
@@ -13,7 +21,7 @@
         <a href="###">有趣</a>
         <a href="###">秒杀</a>
       </nav>
-      <div class="sort">
+      <div class="sort" v-show="isHomeShow || isLoginShow">
         <div class="all-sort-list2">
           <div
             class="item bo"
@@ -496,6 +504,12 @@ import { mapState, mapActions } from "vuex";
 
 export default {
   name: "TypeNav",
+  data() {
+    return {
+      isHomeShow: this.$route.path === "/",
+      isLoginShow: false,
+    };
+  },
   computed: {
     ...mapState({
       navData: (state) => state.home.navData,
